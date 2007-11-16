2e9a162f344683bd6252e6799ab1f20722c21af4
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2007-11-16 14:57:38 +0000

    V3.99

--- src/lib/bsc1.f
+++ src/lib/bsc1.f
@@ -162,8 +162,8 @@ C
       ENDIF
       CALL GUTIME(T)
       IF(ICH.NE.0) WRITE(6,601) T-TOPEN
-  601 FORMAT(1H ,'# GSAF V3.98 : ',
-     &           'Copyright (C) 1983-2006 A. Fukuyama and T. Akutsu'/
+  601 FORMAT(1H ,'# GSAF V3.99 : ',
+     &           'Copyright (C) 1983-2007 A. Fukuyama and T. Akutsu'/
      &       1H ,'#              CLOSED.  USED CPU TIME =',
      &           F10.3,' SEC')
       RETURN
