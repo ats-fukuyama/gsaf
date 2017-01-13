8bc39bc22ce380039419b60d7abb7d6a6e28160f
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2017-01-13 14:05:16 +0900

    nifs/psfx added

--- src/lib/bsc1.f
+++ src/lib/bsc1.f
@@ -166,7 +166,7 @@ C
       CALL GUTIMES(TU,TS,TCU,TCS,TELP)
       IF(ICH.NE.0) WRITE(6,601) TELP-TELPOPEN,TU-TUOPEN,TS-TSOPEN,
      &                          TCU-TCUOPEN,TCS-TCSOPEN
-  601 FORMAT(1H ,'# GSAF V4.05 : ',
+  601 FORMAT(1H ,'# GSAF V4.06 : ',
      &           'Copyright (C) 1983-2014 A. Fukuyama and T. Akutsu'/
      &       1H ,'# CLOSED.      ELAPSED TIME =',
      &            F10.3,' SEC'/
