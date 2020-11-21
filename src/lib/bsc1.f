c1b8aad2c397c67dc62bbf2513c17f0c1e321a86
Atsushi Fukuyama <fukuyama@afportal.net>
2020-11-21 15:41:21 +0900

    V4.13: bsc1.f GNULEN corrected

--- src/lib/bsc1.f
+++ src/lib/bsc1.f
@@ -1,7 +1,7 @@
 C     $Id$C
 C
 C     *****************************************************
-C     ****** GSAF BASIC ROUTINES V4.12 : INTERFACE 1 ******
+C     ****** GSAF BASIC ROUTINES V4.13 : INTERFACE 1 ******
 C     *****************************************************
 C
 C     ****** SYSTEM INITIALIZE ******
@@ -180,7 +180,7 @@ C
       CALL GUTIMES(TU,TS,TCU,TCS,TELP)
       IF(ICH.NE.0) WRITE(6,601) TELP-TELPOPEN,TU-TUOPEN,TS-TSOPEN,
      &                          TCU-TCUOPEN,TCS-TCSOPEN
-  601 FORMAT(1H ,'# GSAF V4.12 : ',
+  601 FORMAT(1H ,'# GSAF V4.13 : ',
      &           'Copyright (C) 1983-2020 A. Fukuyama and T. Akutsu'/
      &       1H ,'# CLOSED.      ELAPSED TIME =',
      &            F10.3,' SEC'/
@@ -1649,7 +1649,7 @@ C     ***** OPTIMUM NUM LENGTH FOR GVALUE *****
 C
       FUNCTION NGULEN(GSTEP)
 C
-      GXL = SNGL(LOG10(DBLE(GSTEP*0.11)))
+      GXL = SNGL(LOG10(DBLE(GSTEP)*0.11D0))
       IF(GXL.LT.0.0) THEN
          NGX = -INT(-GXL)
       ELSE
